da6550d1f1fb69d1bcab73268a240b66d8a2bb74
Ayush <ydvayush27@gmail.com>
2025-06-17 05:21:53 +0530

    Update Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -109,10 +109,7 @@ export default function Header() {
                 >
                   <MessageLogo />
                   <span>Messages</span>
-                  {totalUnreadMessages >= 0 && (
-                    <span className="absolute -top-1 -right-2 bg-red-500 text-white text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center animate-bounce-once">
-                      {totalUnreadMessages}
-                    </span>
+                  </span>
                   )}
                 </Link>
                 <Link
